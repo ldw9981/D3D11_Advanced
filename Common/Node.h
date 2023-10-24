5b0575427e527d978cfa78a31747cf1443da0dbb
LeeDongWon <ldw9981@gmail.com>
2023-10-24 23:24:17 +0900

    설명 수정

--- Common/Node.h
+++ Common/Node.h
@@ -15,7 +15,7 @@ public:
 	std::vector<Node> m_Children;		// 계층 구조를 이루는 자식 노드
 	std::vector<UINT> m_MeshIndices;	// 해당 노드와 연결되는 메시 인덱스
 	Node* m_pParent = nullptr;
-	NodeAnimation* m_pNodeAnimation = nullptr;
+	NodeAnimation* m_pNodeAnimation = nullptr; // 노드가 사용할 NodeAnimation가 있는 주소
 
 	void Create(Model* model,aiNode* node);
 	void UpdateAnimation(float progressTime);
